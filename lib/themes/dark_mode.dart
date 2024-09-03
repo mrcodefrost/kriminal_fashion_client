ff4db0ec618e96d39c2b7db58b27f219fe009a37
mrcodefrost <mr.codefrost@gmail.com>
2024-09-03 10:38:19 +0530

    deprecated bg in theme fixed

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 ThemeData darkMode = ThemeData(
     fontFamily: GoogleFonts.comfortaa().fontFamily,
     colorScheme: const ColorScheme.dark(
-      background: Color.fromARGB(255, 20, 20, 20),
+      surface: Color.fromARGB(255, 20, 20, 20),
       primary: Color.fromARGB(255, 122, 122, 122),
       secondary: Color.fromARGB(255, 30, 30, 30),
       tertiary: Color.fromARGB(255, 47, 47, 47),
--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 ThemeData lightMode = ThemeData(
     fontFamily: GoogleFonts.comfortaa().fontFamily,
     colorScheme: const ColorScheme.light(
-      background: Color(0xffFAFAFF), // lightest
+      surface: Color(0xffFAFAFF), // lightest
       tertiary: Color(0xffEEF0F2), // 2nd lightest
       primary: Color(0xff000000), // 3rd
       secondary: Color(0xff252323), // 4th
