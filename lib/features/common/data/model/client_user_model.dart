17b46b46290cd4bf06b466e0e78e74bb73cb534c
mrcodefrost <mr.codefrost@gmail.com>
2024-09-27 16:04:30 +0530

    user model corrected

--- lib/features/common/data/model/client_user_model.dart
+++ lib/features/common/data/model/client_user_model.dart
@@ -1,29 +1,26 @@
-import 'package:json_annotation/json_annotation.dart';
-
-part 'client_user_model.g.dart';
-
-@JsonSerializable()
 class ClientUser {
-  @JsonKey(name: "id")
   String? id;
-
-  @JsonKey(name: "name")
   String? name;
-
-  @JsonKey(name: "number")
   int? number;
-
-  @JsonKey(name: "email")
   String? email;
 
-  ClientUser({
-    this.id,
-    this.name,
-    this.number,
-    this.email,
-  });
+  ClientUser({this.id, this.name, this.number, this.email});
 
-  factory ClientUser.fromJson(Map<String, dynamic> json) =>
-      _$ClientUserFromJson(json);
-  Map<String, dynamic> toJson() => _$ClientUserToJson(this);
+  factory ClientUser.fromJson(Map<String, dynamic> json) {
+    return ClientUser(
+      id: json['id'],
+      name: json['name'],
+      number: json['number'],
+      email: json['email'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['number'] = number;
+    data['email'] = email;
+    return data;
+  }
 }
