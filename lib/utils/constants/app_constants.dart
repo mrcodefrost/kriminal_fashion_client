0c48c03a856a80836a23f33973d7d42ab29dd2dc
mrcodefrost <mr.codefrost@gmail.com>
2024-10-15 17:33:28 +0530

    fading image updated

--- lib/utils/constants/app_constants.dart
+++ lib/utils/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppImages {
   static const String demoImageURL =
       'https://static.zara.net/assets/public/c5d0/cda6/3fbd4a5fa232/e4b313d6aa37/08372090800-p/08372090800-p.jpg?ts=1708613834317&w=563';
 
-  static const String splashLogo = 'assets/images/splashLogo.png';
+  static const String splashLogo = 'assets/images/splashLogo.jpg';
 }
 
 class AppSecrets {
