9822ec9997783c62d772d3a501e899c548293825
mrcodefrost <mr.codefrost@gmail.com>
2024-11-29 15:40:20 +0530

    cart crud skeleton func added

--- lib/features/cart/presentation/controller/cart_controller.dart
+++ lib/features/cart/presentation/controller/cart_controller.dart
@@ -1,10 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:kriminal_fashion_client/utils/globals.dart';
 
 import '../../../product/data/model/product.dart';
 
 class CartController extends GetxController {
-  var selectedIndex = 0.obs;
+  // ====== INSTANCES ====== //
 
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late CollectionReference userCollection;
+  late CollectionReference cartCollection;
+  late CollectionReference wishlistCollection;
+
+  var selectedIndex = 0.obs;
   RxList<Product> wishListedProducts = <Product>[].obs;
 
   void switchView(int index) {
@@ -22,6 +30,35 @@ class CartController extends GetxController {
   bool isProductWishListed(Product product) {
     return wishListedProducts.contains(product);
   }
+
+  // ====== STATES ====== //
+
+  @override
+  void onInit() {
+    super.onInit();
+    userCollection = firestore.collection('users');
+  }
+
+  // ====== CART CRUD ====== //
+
+  // Future<void> addToCart() async {
+  //   try {
+  //     // Generate a unique ID for the cart item
+  //     DocumentReference cartDocRef =
+  //         userCollection.doc(userId).collection('cartItems').doc(); // Generate a unique cart item ID
+  //
+  //     cartItem.id = cartDocRef.id; // Assign the generated ID to the cart item
+  //
+  //     // Save the cart item to Firestore
+  //     cartDocRef.set(cartItem.toJson()).then((_) {
+  //       Get.snackbar('Success', 'Cart item added successfully', colorText: Colors.green);
+  //     });
+  //   } catch (e) {
+  //     logg.e('Error adding to cart: $e');
+  //   }
+  // }
+
+  Future<void> removeFromCart() async {}
 
   // Future<void> fetchWishListedProducts() async {
   //   try {
