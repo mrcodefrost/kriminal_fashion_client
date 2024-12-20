b54eb20ae0b39e382ba7d74d9e7f58a5aa0e6d0c
mrcodefrost <mr.codefrost@gmail.com>
2024-12-20 10:57:49 +0530

    Tapping wishlist in my account will now open wishlist directly

--- lib/features/account/presentation/view/screens/account_screen.dart
+++ lib/features/account/presentation/view/screens/account_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:kriminal_fashion_client/features/cart/presentation/controller/cart_controller.dart';
 import 'package:kriminal_fashion_client/features/cart/presentation/view/screens/cart_screen.dart';
 
 import '../../../../../themes/theme_controller.dart';
@@ -13,105 +14,171 @@ class AccountScreen extends StatelessWidget {
   AccountScreen({super.key});
 
   final authController = Get.find<AuthController>();
+  final cartController = Get.find<CartController>();
 
   // List of options with title and icon // incorrect way since UI rebuild will not occur
-  final List<Map<String, dynamic>> accountOptions = [
-    {
-      'title': 'Purchases',
-      'icon': Icons.shopping_bag,
-    },
-    {
-      'title': 'Wishlist',
-      'icon': Icons.favorite,
-    },
-    {
-      'title': 'Notifications',
-      'icon': Icons.notifications,
-    },
-    {
-      'title': 'Payment Methods',
-      'icon': Icons.payment,
-    },
-    {
-      'title': 'Profile',
-      'icon': Icons.person,
-    },
-    {
-      'title': 'Settings',
-      'icon': Icons.settings,
-    },
-    {
-      'title': 'Help',
-      'icon': Icons.help_outline,
-    },
-    {
-      'title': 'Toggle theme',
-      'icon': Icons.lightbulb,
-    },
-    {
-      'title': 'Sign out',
-      'icon': Icons.logout,
-    },
-  ];
+  // final List<Map<String, dynamic>> accountOptions = [
+  //   {
+  //     'title': 'Purchases',
+  //     'icon': Icons.shopping_bag,
+  //   },
+  //   {
+  //     'title': 'Wishlist',
+  //     'icon': Icons.favorite,
+  //   },
+  //   {
+  //     'title': 'Notifications',
+  //     'icon': Icons.notifications,
+  //   },
+  //   {
+  //     'title': 'Payment Methods',
+  //     'icon': Icons.payment,
+  //   },
+  //   {
+  //     'title': 'Profile',
+  //     'icon': Icons.person,
+  //   },
+  //   {
+  //     'title': 'Settings',
+  //     'icon': Icons.settings,
+  //   },
+  //   {
+  //     'title': 'Help',
+  //     'icon': Icons.help_outline,
+  //   },
+  //   {
+  //     'title': 'Toggle theme',
+  //     'icon': Icons.lightbulb,
+  //   },
+  //   {
+  //     'title': 'Sign out',
+  //     'icon': Icons.logout,
+  //   },
+  // ];
 
-  final List<AccountsTile> accountTiles = [
-    AccountsTile(
-      title: 'Purchases',
-      icon: Icons.shopping_cart,
-      onTap: () {
-        Get.to(() => const PurchasesScreen());
-      },
-    ),
-    AccountsTile(
-      title: 'Wishlist',
-      icon: Icons.favorite,
-      onTap: () {
-        Get.to(() => CartOrWishlistScreen());
-      },
-    ),
-    AccountsTile(
-      title: 'Notifications',
-      icon: Icons.notifications,
-      onTap: () {
-        // Get.to(() => const NotificationsScreen());
-      },
-    ),
-    AccountsTile(
-      title: 'Payment Methods',
-      icon: Icons.payment,
-      onTap: () {
-        // Get.to(() => const PaymentMethodsScreen());
-      },
-    ),
-    AccountsTile(
-      title: 'Profile',
-      icon: Icons.person,
-      onTap: () {
-        Get.to(() => const ProfileScreen());
-      },
-    ),
-    AccountsTile(
-      title: 'Settings',
-      icon: Icons.settings,
-      onTap: () {
-        // Get.to(() => const SettingsScreen());
-      },
-    ),
-    AccountsTile(
-      title: 'Help',
-      icon: Icons.help,
-      onTap: () {
-        // Get.to(() => const HelpScreen());
-      },
-    ),
-    AccountsTile(
-      title: 'Toggle theme',
-      icon: Icons.lightbulb_outline,
-      onTap: () {
-        ThemeController.toggleThemeMode();
-      },
-    ),
-  ];
+  // Use a getter method to lazily initialize the list.
+  // This allows cartController to be accessed properly since it's initialized beforehand.
+  List<AccountsTile> get accountTiles => [
+        AccountsTile(
+          title: 'Purchases',
+          icon: Icons.shopping_cart,
+          onTap: () {
+            Get.to(() => const PurchasesScreen());
+          },
+        ),
+        AccountsTile(
+          title: 'Wishlist',
+          icon: Icons.favorite,
+          onTap: () {
+            // Set the cart screen index to 1 to display wishlist
+            cartController.switchView(1);
+            Get.to(() => CartOrWishlistScreen());
+          },
+        ),
+        AccountsTile(
+          title: 'Notifications',
+          icon: Icons.notifications,
+          onTap: () {
+            // Get.to(() => const NotificationsScreen());
+          },
+        ),
+        AccountsTile(
+          title: 'Payment Methods',
+          icon: Icons.payment,
+          onTap: () {
+            // Get.to(() => const PaymentMethodsScreen());
+          },
+        ),
+        AccountsTile(
+          title: 'Profile',
+          icon: Icons.person,
+          onTap: () {
+            Get.to(() => const ProfileScreen());
+          },
+        ),
+        AccountsTile(
+          title: 'Settings',
+          icon: Icons.settings,
+          onTap: () {
+            // Get.to(() => const SettingsScreen());
+          },
+        ),
+        AccountsTile(
+          title: 'Help',
+          icon: Icons.help,
+          onTap: () {
+            // Get.to(() => const HelpScreen());
+          },
+        ),
+        AccountsTile(
+          title: 'Toggle theme',
+          icon: Icons.lightbulb_outline,
+          onTap: () {
+            ThemeController.toggleThemeMode();
+          },
+        ),
+      ];
+
+  // final List<AccountsTile> accountTiles = [
+  //   AccountsTile(
+  //     title: 'Purchases',
+  //     icon: Icons.shopping_cart,
+  //     onTap: () {
+  //       Get.to(() => const PurchasesScreen());
+  //     },
+  //   ),
+  //   AccountsTile(
+  //     title: 'Wishlist',
+  //     icon: Icons.favorite,
+  //     onTap: () {
+  //       // Set the cart screen index to 1 to display wishlist
+  //       cartController.switchView(1);
+  //       Get.to(() => CartOrWishlistScreen());
+  //     },
+  //   ),
+  //   AccountsTile(
+  //     title: 'Notifications',
+  //     icon: Icons.notifications,
+  //     onTap: () {
+  //       // Get.to(() => const NotificationsScreen());
+  //     },
+  //   ),
+  //   AccountsTile(
+  //     title: 'Payment Methods',
+  //     icon: Icons.payment,
+  //     onTap: () {
+  //       // Get.to(() => const PaymentMethodsScreen());
+  //     },
+  //   ),
+  //   AccountsTile(
+  //     title: 'Profile',
+  //     icon: Icons.person,
+  //     onTap: () {
+  //       Get.to(() => const ProfileScreen());
+  //     },
+  //   ),
+  //   AccountsTile(
+  //     title: 'Settings',
+  //     icon: Icons.settings,
+  //     onTap: () {
+  //       // Get.to(() => const SettingsScreen());
+  //     },
+  //   ),
+  //   AccountsTile(
+  //     title: 'Help',
+  //     icon: Icons.help,
+  //     onTap: () {
+  //       // Get.to(() => const HelpScreen());
+  //     },
+  //   ),
+  //   AccountsTile(
+  //     title: 'Toggle theme',
+  //     icon: Icons.lightbulb_outline,
+  //     onTap: () {
+  //       ThemeController.toggleThemeMode();
+  //     },
+  //   ),
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -199,13 +266,6 @@ class AccountScreen extends StatelessWidget {
           ),
         ],
       ),
-
-      // body: ListView.builder(
-      //   itemCount: accountTiles.length,
-      //   itemBuilder: (context, index) {
-      //     return accountTiles[index];
-      //   },
-      // ),
     );
   }
 }
